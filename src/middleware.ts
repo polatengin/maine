d0e8dad537ce65a4d3d5b44646762e6af461602d
polatengin <enpolat@microsoft.com>
2023-11-23 18:53:12 +0000

    adding csp header for script elements

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ export function middleware(request: NextRequest) {
   const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
   const cspHeader = `
     default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'unsafe-eval' https://www.googletagmanager.com/;
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
